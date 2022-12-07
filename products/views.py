78960c1adf5fcffe08cc5b4f563431d52c41d374
Nigel <pratyushnbaxla79@gmail.com>
2022-12-07 18:07:08 +0000

    Filter Category

--- products/views.py
+++ products/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 
 class ProductsView(ListView):
     model = Product
     template_name = "products/product_list.html"
-    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categorys"] = Category.objects.all()
+        return context
 
 
 class ProductDetail(DetailView):
@@ -22,3 +26,8 @@ class FilteredView(ListView):
     def get_queryset(self):
         products = Product.objects.filter(category__slug=self.kwargs['slug'])
         return products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categorys"] = Category.objects.all()
+        return context
